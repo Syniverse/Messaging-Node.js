12169d9cc4245e31522ebba5a25e4a8103f44820
Kola Layokun <Syniverse1@users.noreply.github.com>
2021-08-17 12:21:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2018 Syniverse Communication Gateway
+ * Copyright (c) 2021 Syniverse Communication Gateway
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
